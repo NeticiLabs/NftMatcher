6515cab69282a2453ba44dfc96bb46ab4a2ada3f
arc0035 <aoronchu@gmail.com>
2022-09-13 16:47:25 +0800

    some training test

--- pkg/experiments/vgg16_test.py
+++ pkg/experiments/vgg16_test.py
@@ -1,26 +1,96 @@
-import tensorflow as tf
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.sans-serif'] = ['SimHei'] # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False # 用来正常显示负号
+
+import os, PIL
+
 import numpy as np
-import keras
-X = np.arange(1, 101, step=0.1)
-y = [x + 10 for x in X]
+np.random.seed(1)
 
-X = tf.cast(tf.constant(X), dtype=tf.float32)
-y = tf.cast(tf.constant(y), dtype=tf.float32)
+import tensorflow as tf
+tf.random.set_seed(1)
+import pathlib
+data_dir = '/Users/chubingnan/Dev/github/arc0035/Recognition-of-eye-state-based-on-Vgg16/Eye_dataset'
+data_dir = pathlib.Path(data_dir)
+print(data_dir)
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(64, input_shape=(1,), activation='relu'),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(1)
-])
+image_count = len(list(data_dir.glob('*/*')))
+print('图片总数为:', image_count)
 
-model.compile(
-    loss=tf.keras.losses.mean_absolute_error,
-    optimizer=tf.keras.optimizers.Adam(learning_rate=0.1),
-    metrics=['mean_absolute_error']
+# 参数准备
+batch_size = 8
+img_height = 224
+img_width = 224
+
+# 数据集-训练集
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset='training',
+    seed=12,
+    image_size=(img_height, img_width),
+    batch_size=batch_size
 )
 
-model.fit(X, y,     epochs=100)
+# 数据集-测试集
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset='validation',
+    seed = 12,
+    image_size=(img_height, img_width),
+    batch_size=batch_size
+)
 
-r = model.predict([10, 20, 30])
+class_names = train_ds.class_names
+print(class_names)
+class_names = val_ds.class_names
+print(class_names)
 
-print(r)
+# plt.figure(figsize=(10,5))
+# plt.suptitle('Data Visualization')
+#
+# for images, labels in train_ds.take(1):
+#     for i in range(8):
+#         ax = plt.subplot(2, 4, i+1)
+#         plt.imshow(images[i].numpy().astype('uint8'))
+#         plt.title(class_names[labels[i]])
+#         plt.savefig('pic1.jpg', dpi=600)
+#         plt.axis('off')
+
+for images_batch, labels_batch in train_ds:
+    print(images_batch.shape)
+    print(labels_batch.shape)
+    break
+
+# config data set
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(AUTOTUNE)
+
+# vgg-16
+model = tf.keras.applications.VGG16()
+model.summary()
+
+initial_learning_rate = 1e-4
+lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate,
+    decay_steps = 20,
+    decay_rate = 0.96,
+    staircase = True
+)
+optimizer = tf.keras.optimizers.Adam(learning_rate = lr_schedule)
+
+model.compile(
+    optimizer = optimizer,
+    loss = 'sparse_categorical_crossentropy',
+    metrics = ['accuracy']
+)
+
+epochs = 10
+history = model.fit(
+    train_ds,
+    validation_data= val_ds,
+    epochs = epochs
+)
+
